a6d3b9218d6b14b47fd630cc71ae51ccfee2fd26
slsius <ssiusinglung@gmail.com>
2020-08-04 16:01:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ cv2.imshow('My Image', thresh4)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((3,3),np.uint8)
 erosion = cv2.erode(img,kernel,iterations = 1)
 cv2.imshow('My Image', erosion)
 cv2.waitKey(0)
