44a90240c66660f9dc6ecbf1956906fa8282c731
slsius <ssiusinglung@gmail.com>
2020-08-04 14:02:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ image_path = r'/Users/siu/Desktop/qr.jpeg '
 img = cv2.imread(image_path,0)
 
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+
 ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
 ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
 ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
 ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 
-titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+cv2.imshow('My Image', thresh1)
+cv2.imshow('My Image', thresh2)
+cv2.imshow('My Image', thresh3)
+cv2.imshow('My Image', thresh4)
+cv2.imshow('My Image', thresh5)
 
-for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-
-plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
