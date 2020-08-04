3db34af329ff47150d7f1c7685081d0d26f88642
slsius <ssiusinglung@gmail.com>
2020-08-04 16:02:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,11 @@ image_path = 'qr.jpeg'
 img = cv2.imread(image_path,0)
 
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-cv2.imshow('My Image', thresh1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-cv2.imshow('My Image', thresh4)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 kernel = np.ones((3,3),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 1)
+erosion = cv2.erode(img,kernel,iterations = 5)
 cv2.imshow('My Image', erosion)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
