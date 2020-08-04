2856f5f0c4cec22527ba1d93aa272e172eed6296
slsius <ssiusinglung@gmail.com>
2020-08-04 14:14:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,39 @@ image_path = 'qr.jpeg'
 
 img = cv2.imread(image_path,0)
 cv2.imshow('My Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-'''
+cv2.imshow('My Image', thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+cv2.imshow('My Image', thresh2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
-'''
-
-cv2.imshow('My Image', thresh1)
-'''
-cv2.imshow('My Image', thresh2)
 cv2.imshow('My Image', thresh3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
 cv2.imshow('My Image', thresh4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 cv2.imshow('My Image', thresh5)
-'''
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
