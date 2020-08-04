b6c9266a9b92264914e3a93d615e7c1963aa612f
slsius <ssiusinglung@gmail.com>
2020-08-04 16:04:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ img = cv2.imread(image_path,0)
 
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+#ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
 
-kernel = np.ones((1,1),np.uint8)
+kernel = np.ones((2,2),np.uint8)
 erosion = cv2.erode(img,kernel,iterations = 1)
 cv2.imshow('My Image', erosion)
 cv2.waitKey(0)
@@ -24,7 +24,9 @@ cv2.imshow('My Image', thresh1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+'''
 ret,thresh4 = cv2.threshold(erosion,127,255,cv2.THRESH_TOZERO)
 cv2.imshow('My Image', thresh4)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
