37d7135ade1f2300ea40ff84d87ada3111103ce9
slsius <ssiusinglung@gmail.com>
2020-08-04 14:15:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,12 +35,3 @@ ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 cv2.imshow('My Image', thresh5)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
