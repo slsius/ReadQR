0ba67521445a4c2c00f21ac2bfa9fa6e80a33fef
slsius <ssiusinglung@gmail.com>
2020-08-06 08:09:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,47 +9,13 @@ import cv2 as cv
 
 # Image path 
 #image_path = '/Users/siu/Documents/PythonPrograms/ReadQR/qr.jpeg'
-image_path = 'qr.jpeg'
+image_path = 'shadow_out.jpeg'
 
 rt = 0.8
 
 img = cv2.imread(image_path,0)
-#image = img 
 
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-
-bw_im = thresh1
-barcodes = decode(bw_im, symbols=[ZBarSymbol.QRCODE])
-image = thresh1
-
-new_image = np.zeros(image.shape, image.dtype)
-alpha = 1.0 # Simple contrast control
-beta = 0    # Simple brightness control
-# Initialize values
-'''
-print(' Basic Linear Transforms ')
-print('-------------------------')
-try:
-    alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
-    beta = int(input('* Enter the beta value [0-100]: '))
-except ValueError:
-    print('Error, not a number')
-'''    
-# Do the operation new_image(i,j) = alpha*image(i,j) + beta
-# Instead of these 'for' loops we could have used simply:
-# new_image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
-# but we wanted to show you how to access the pixels :)
-'''
-for y in range(image.shape[0]):
-    for x in range(image.shape[1]):
-        for c in range(image.shape[2]):
-            new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
-cv.imshow('Original Image', image)
-cv.imshow('New Image', new_image)
-# Wait until user press some key
-cv.waitKey()
-cv2.destroyAllWindows()
-'''
+ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
 rgb_planes = cv2.split(img)
 
@@ -68,8 +34,6 @@ result_norm = cv2.merge(result_norm_planes)
 
 cv2.imwrite('shadows_out.png', result)
 cv2.imwrite('shadows_out_norm.png', result_norm)
-
-
 
 
 bw_im = result_planes
